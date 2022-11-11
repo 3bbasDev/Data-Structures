ba1fa2a5428fdec1dc3fe60bcafe9525ae90784c
Abbas K. Radam <abbasabbe1995@gmail.com>
2022-11-11 20:59:31 +0300

    Initial commit

---
+++ Data Structures/Runs/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    public static int x = 1;
+    private static void Main(string[] args)
+    {
+        StackRun();
+    }
+
+    private static void StackRun()
+    {
+        Stack.Stack<int> stack = new(10);
+
+        stack.Puch(1);
+        stack.Puch(2);
+        stack.Puch(3);
+        stack.Puch(4);
+        stack.Pop();
+        stack.GetTop(ref x);
+        stack.Puch(9);
+        stack.Print();
+    }
+}
---
+++ Data Structures/Stack/Stack.cs
@@ -0,0 +1,64 @@
+﻿namespace Stack
+{
+    public class Stack<T>
+    {
+        private int top;
+        private T[] item;
+        private int stachSize = 0;
+        public Stack(int stachSize)
+        {
+            this.stachSize = stachSize;
+            item = new T[stachSize];
+            top = -1;
+        }
+
+        public void Puch(T element)
+        {
+            if (top >= stachSize - 1)
+                Console.WriteLine("Stack full on push");
+            else
+            {
+                top++;
+                item[top] = element;
+            }
+        }
+        public bool IsEmpty() => top < 0;
+
+        public void Pop()
+        {
+            if (IsEmpty())
+                Console.WriteLine("Stack empty on pup");
+            else top--;
+        }
+        public void Pop(ref T element)
+        {
+            if (IsEmpty())
+                Console.WriteLine("Stack empty on pup");
+            else
+            {
+                element = item[top];
+                top--;
+            }
+        }
+
+        public void GetTop(ref T stackTop)
+        {
+            if (IsEmpty())
+                Console.WriteLine("Stack empty on pup");
+            else
+            {
+                stackTop = item[top];
+                Console.WriteLine(stackTop);
+            }
+        }
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = top; i >= 0; i--)
+            {
+                Console.Write(item[i] + " ");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
